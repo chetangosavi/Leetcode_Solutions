7a01f498e483dc713c018110c1cdcf1997dd1c14
Chetan Gosavi <chetangosavi81@gmail.com>
2024-12-02 18:05:03 +0530

    solutions

---
+++ leetcodesolutions/src/leetcodesolutions/arrays/SeccondLargeNumber.java
@@ -0,0 +1,23 @@
+package leetcodesolutions.arrays;
+
+public class SeccondLargeNumber {
+
+	public static void main(String[] args) {
+		int[] nums = {12, 35, 1, 10, 34, 1};
+		int n = nums.length;
+        int max = -1;
+        int prev = -1;
+        
+        for(int i = 0; i<n; i++) {
+        	if(nums[i]>max) {
+        		prev = max;
+        		max = nums[i];
+        	}
+        	if(nums[i]>prev && nums[i]<max) {
+        		prev = nums[i];
+        	}
+        }
+        System.out.println(prev);
+	}
+
+}
---
+++ leetcodesolutions/src/leetcodesolutions/arrays/SecongLargestNumber.java
@@ -0,0 +1,26 @@
+package leetcodesolutions.arrays;
+
+public class SecongLargestNumber {
+
+	public static void main(String[] args) {
+		int[] nums = {12, 35, 1, 10, 34, 1};
+		int n = nums.length;
+        int max = 0;
+        int secMax = 0;
+        
+        //calculating max in the array
+        for(int i = 0; i < n; i++){
+            if(nums[i]>max){
+                max = nums[i];
+            }
+        }
+        for(int i = 0; i<n;i++) {
+        	if(nums[i]>secMax && nums[i]!= max) {
+        		secMax = nums[i];
+        	}
+        }
+        System.out.println(secMax);
+        System.out.println(max);
+	}
+
+}

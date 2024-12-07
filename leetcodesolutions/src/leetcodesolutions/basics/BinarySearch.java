87a622bc244193149c801a9c8dff9912cf77b325
Chetan Gosavi <chetangosavi81@gmail.com>
2024-12-08 00:21:23 +0530

    Binary Search

--- leetcodesolutions/src/leetcodesolutions/basics/BinarySearch.java
+++ leetcodesolutions/src/leetcodesolutions/basics/BinarySearch.java
@@ -19,27 +19,27 @@ public class BinarySearch {
 				4) Repeat steps 2-3 until low becomes greater than high.
 		 */
 
-	int[] arr = {1,2,3,4,5,6,7,8,9};
-	int n = arr.length;
-	int target = 4;
-	int left = 0;
-	int right = n-1;
-	
-	while(left<=right) {
-		int mid =(left + right)/2;
-		if(arr[mid] == target) {
-			System.out.println(mid);
-			return;
-		}
-		if(arr[mid]<target) {
-			left = mid+1;
-		}
-		else {
-			right = mid-1;
-		}
-	}
-	
-	System.out.println(-1);
+				int[] arr = {1,2,3,4,5,6,7,8,9};
+				int n = arr.length;
+				int target = 4;
+				int left = 0;
+				int right = n-1;
+				
+				while(left<=right) {
+					int mid =(left + right)/2;
+					if(arr[mid] == target) {
+						System.out.println(mid);
+						return;
+					}
+					if(arr[mid]<target) {
+						left = mid+1;
+					}
+					else {
+						right = mid-1;
+					}
+				}
+				
+				System.out.println(-1);
   }
 
 }

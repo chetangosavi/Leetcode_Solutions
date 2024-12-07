e2733780754f00701e371b97153b109a3ca392d6
Chetan Gosavi <chetangosavi81@gmail.com>
2024-12-08 01:17:03 +0530

    Binary search

--- leetcodesolutions/src/leetcodesolutions/basics/BinarySearch.java
+++ leetcodesolutions/src/leetcodesolutions/basics/BinarySearch.java
@@ -40,6 +40,55 @@ public class BinarySearch {
 				}
 				
 				System.out.println(-1);
+				
+				/*
+				  To avoid integer overflow:
+				  			
+				  	left + (right - left)/2 formula is used
+				  			bec  while int value is close to Integer_MAX and if we try to add them directly
+				  			integer will overflow so to avoid that this formula is used
+				  	
+				  	
+				  	DRY RUN :
+				  	                     0 1 2 3 4 5 6 7 
+				  			int[] arr = {1,2,3,4,5,6,7,8};
+				  			int target = 5
+				  			n = 8;
+				  			left = 0;
+				  			right = n-1;  8-1 = 7
+				  			IT - 1 :
+				  				(left+right)/2 ;
+				  				int mid  = (7+0)/2 => 7/2 => 3
+				  				
+				  				arr[mid] == target ? arr[3] == 5 ? 4 == 5 ? no 
+				  				arr[mid] < target ? 4 < 5 => yes
+				  				left = mid + 1 = 3 + 1 = 4
+				  				
+				  			IT - 2:
+				  				left = 4  right = 7
+				  				
+				  				mid = (7+4)/2 = 11 / 2 = 5
+				  				arr[5] == target ? 6 == 5 ? no
+				  				arr[5]<target => 6 < 5 ? no
+				  				right = mid - 1 = 5 - 1 = 4
+				  				
+				  			IT - 3:
+				  			
+				  				right = 4 left = 4
+				  				mid = 4
+				  				arr[mid] == target ? arr[4] == 5 ? 5 == 5 ? true;
+				  				return mid;
+				  				output : 4
+				  				
+				  		In case we use :
+				  		
+				  			left + (Right - left)/2
+				  			 result for iteration 2 : lets cross check and understand
+				  			 
+				  			 4 + (7-4)/2
+				  			 4 + 3/2 = 4 + 1 = 5  
+				  			
+				 */
   }
 
 }

987f95b52cd64e8aa14a7abc377e26ff24037d44
Chetan Gosavi <chetangosavi81@gmail.com>
2024-12-24 16:21:32 +0530

    LinkedList

--- leetcodesolutions/src/leetcodesolutions/basics/LinkedListInDetail.java
+++ leetcodesolutions/src/leetcodesolutions/basics/LinkedListInDetail.java
@@ -1,4 +1,5 @@
 package leetcodesolutions.basics;
+import java.util.LinkedList;
 
 public class LinkedListInDetail {
 
@@ -11,8 +12,30 @@ public class LinkedListInDetail {
 		 	element is a separate object with a data part and address part. The elements are linked 
 		 	using pointers and addresses and each element is known as a node.
 		 
+		 When to use linkedlist vs arraylist:
+		 
+		 	ArrayList - when retrival of data is priority rather than just insertion and deletion
+		 	else use LinkedList
+		 	
+		 Syntax to create linkedlist:
+		 
+		 		LinkedList<Integer> li = new LinkedList<>();
+		 		
+		 Methods:
+		 
+		 		add() : used to insert elements into list
+		 		
+		 Note: It also has a few disadvantages like the nodes cannot be accessed directly instead 
+		 we need to start from the head and follow through the link to reach a node we wish to access.
+		 
 		 */ 
 
+		LinkedList<Integer> li = new LinkedList<Integer>();
+		li.add(1);
+		li.add(2);
+		li.add(3);
+		
+		System.out.println("Linked List: "+li);
 	}
 
 }

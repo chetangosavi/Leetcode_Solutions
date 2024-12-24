188c75d9cefaf7380dbec70a828978676daa8024
Chetan Gosavi <chetangosavi81@gmail.com>
2024-12-24 15:34:21 +0530

    LinkedList

--- leetcodesolutions/src/leetcodesolutions/basics/LinkedListInDetail.java
+++ leetcodesolutions/src/leetcodesolutions/basics/LinkedListInDetail.java
@@ -5,7 +5,11 @@ public class LinkedListInDetail {
 	public static void main(String[] args) {
 		/*
 		 LinkedList:
-		 	
+		 	Linked List is a part of the Collection framework present in java.util package. 
+		 	This class is an implementation of the LinkedList data structure which is a linear 
+		 	data structure where the elements are not stored in contiguous locations and every 
+		 	element is a separate object with a data part and address part. The elements are linked 
+		 	using pointers and addresses and each element is known as a node.
 		 
 		 */ 
 

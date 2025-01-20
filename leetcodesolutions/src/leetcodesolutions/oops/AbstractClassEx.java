a1cce1cebc45c3cd1568da173080d788ab0d0b06
Chetan Gosavi <chetangosavi81@gmail.com>
2025-01-20 20:48:02 +0530

    "Java Basics"

--- leetcodesolutions/src/leetcodesolutions/oops/AbstractClassEx.java
+++ leetcodesolutions/src/leetcodesolutions/oops/AbstractClassEx.java
@@ -1,5 +1,14 @@
 package leetcodesolutions.oops;
 
 public class AbstractClassEx {
-
+	public static void main(String args) {
+		/*
+		 	Main Difference btwn Abstract class and Interface :
+		 	
+		 			- class can implement multiple interface but only one class at a time
+		 			- in interface methods are assumed to be abstract
+		 			- abstract class can have data members as well as methods or abstract methods
+		 			- data members inside interface are static and final
+		 */
+	}
 }

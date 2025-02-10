469175113dd66feacccc9002380adc82bcbcf0e1
Chetan Gosavi <chetangosavi81@gmail.com>
2025-02-11 01:57:21 +0530

    LinkedList

--- leetcodesolutions/src/leetcodesolutions/basics/LinkedListInDetail.java
+++ leetcodesolutions/src/leetcodesolutions/basics/LinkedListInDetail.java
@@ -1,42 +1,103 @@
 package leetcodesolutions.basics;
-import java.util.LinkedList;
+
+class Node{
+	String name;
+	String details;
+	Node next;
+	Node(){}
+	Node(String name, String details){
+		this.name = name;
+		this.details = details;
+	}
+	
+}
+class LinkedList{
+	Node head;
+	
+	LinkedList(){
+		this.head = null;
+	}
+	
+	void addFirstCustomer(String name, String details) {
+	    head = new Node(name, details);
+	}
+	
+	void addCustomers(String name, String details) {
+		Node newNode = new Node(name,details);
+		if(head==null) {
+			newNode = head;
+		}
+		else {
+			Node current = head;
+			while(current.next != null) {
+				current = current.next;
+			}
+			current.next = newNode;
+		}
+	}
+	
+	void removeCustomer(String customerName) {
+		if(head == null) return;
+		if(head.name.equals(customerName)) {
+			head = head.next;
+			return;
+		}
+		Node current = head;
+		while(current.next != null && !current.next.name.equals(customerName) ) {
+			current = current.next;
+		}
+		if(current.next != null ) {
+			current.next = current.next.next;
+		}
+	}
+	
+	void upadateCustomer(String customerName, String newDetails) {
+		Node current = head;
+		while(current!=null) {
+			if(current.name.equals(customerName)) {
+				current.details = newDetails;
+				return;
+			}
+			current = current.next;
+		}
+	}
+	
+	void displayLinkedList() {
+		Node current = head;
+		
+		while(current!=null) {
+			System.out.println("Name : "+current.name +"\nDetails: "+current.details);
+			System.out.println("");
+			current = current.next;
+		}
+	}
+	
+	void addVIPCustomer(String name, String details) {
+		
+		Node newVIPNode = new Node(name ,details);
+		newVIPNode.next = head;
+		head = newVIPNode;
+		
+	}
+}
 
 public class LinkedListInDetail {
 
 	public static void main(String[] args) {
-		/*
-		 LinkedList:
-		 	Linked List is a part of the Collection framework present in java.util package. 
-		 	This class is an implementation of the LinkedList data structure which is a linear 
-		 	data structure where the elements are not stored in contiguous locations and every 
-		 	element is a separate object with a data part and address part. The elements are linked 
-		 	using pointers and addresses and each element is known as a node.
-		 
-		 When to use linkedlist vs arraylist:
-		 
-		 	ArrayList - when retrival of data is priority rather than just insertion and deletion
-		 	else use LinkedList
-		 	
-		 Syntax to create linkedlist:
-		 
-		 		LinkedList<Integer> li = new LinkedList<>();
-		 		
-		 Methods:
-		 
-		 		add() : used to insert elements into list
-		 		
-		 Note: It also has a few disadvantages like the nodes cannot be accessed directly instead 
-		 we need to start from the head and follow through the link to reach a node we wish to access.
-		 
-		 */ 
-
-		LinkedList<Integer> li = new LinkedList<Integer>();
-		li.add(1);
-		li.add(2);
-		li.add(3);
+		LinkedList waitlist = new LinkedList();
 		
-		System.out.println("Linked List: "+li);
+		waitlist.addFirstCustomer("Alice","Single");
 		
+		waitlist.addCustomers("Bob", "Single");
+		waitlist.addCustomers("Smitha", "Married");
+		
+		waitlist.removeCustomer("alice");
+		
+		waitlist.upadateCustomer("Alice","Married");
+		
+		waitlist.addVIPCustomer("VIP Dave","Single");
+		waitlist.addCustomers("Surya", "Married");
+		waitlist.displayLinkedList();
 		
 	}
 

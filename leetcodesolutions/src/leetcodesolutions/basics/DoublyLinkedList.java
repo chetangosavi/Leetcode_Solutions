d99835ca8f94c2d8f173287e88623e9e90c8c58b
Chetan Gosavi <chetangosavi81@gmail.com>
2025-02-13 02:16:58 +0530

    doubly linkedlist

--- leetcodesolutions/src/leetcodesolutions/basics/DoublyLinkedList.java
+++ leetcodesolutions/src/leetcodesolutions/basics/DoublyLinkedList.java
@@ -14,9 +14,54 @@ public class DoublyLinkedList {
 		}
 	}
 	Node head;
+	Node tail;
 	
 	DoublyLinkedList(){
 		this.head = null;
+		this.tail = null;
+	}
+	
+	void addNodeAtHead(int data) {
+		Node newNode = new Node(data);
+		if(head == null) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			newNode.next = head;
+			head.prev = newNode;
+			head = newNode;
+		}
+	}
+	
+	void addNodeAtTail(int data) {
+		Node newNode = new Node(data);
+		if(tail == null) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			newNode.next = tail;
+			tail.next = newNode;
+			tail=newNode;
+		}
+	}
+	
+	void addNodeInMiddle(Node prevNode, int data) {
+		if(prevNode == null) {
+			return;
+		}
+		
+		Node newNode = new Node(data);
+		newNode.next = prevNode.next;
+		prevNode.next = newNode;
+		newNode.prev = prevNode;
+		if(newNode.next!=null) {
+			newNode.next.prev = newNode;
+		}
+		else{
+			tail = newNode;
+		}
 	}
 	
 	public static void main(String[] args) {
